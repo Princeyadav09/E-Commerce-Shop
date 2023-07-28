922994e3bc53e1332db92ba8a381a245f448e6fe
[princeyadav09] <[princekumaryadav09@gmail.com]>
2023-07-28 14:01:53 +0530

    socket url

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "http://localhost:8000/";
+const ENDPOINT = "https://socket-e-shop.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "http://localhost:8000/";
+const ENDPOINT = "https://socket-e-shop.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
